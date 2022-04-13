777ede0dbf67337d1a28a5f9d8cf38308431a91a
sanya buryat <alexandrbnimaev@yandex.ru>
2022-04-14 02:35:58 +0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ let app = require("express")();
 const MongoClient = require("mongodb").MongoClient;
 const mongoClient = new MongoClient("mongodb://alexandr:baguvix1B@45.143.95.183:27017");
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(morgan("dev"));
 
 let http = require("http").Server(app);
@@ -13,23 +15,39 @@ let http = require("http").Server(app);
 mongoClient.connect(function (err, client) {
   if (err) return console.log(err);
   app.locals.collection = client.db("axbn").collection("users");
+  client.db("axbn").collection("users").createIndex({"username": 1}, {unique: true})
   http.listen(3000, function () {
     console.log("Сервер ожидает подключения...");
   });
 });
 
 app.post('/user/register', async function (req, res) {
-  if (req.query.name == null) {
-    return res.sendStatus(400)
-  }
-  console.log(req.body)
+  if (req.query.username == null) return res.sendStatus(400)
+  
   const collection = app.locals.collection
+  
   await collection.insertOne(req.query).then((data) => {
-    res.send(true)
+    res.send(data)
   }).catch(err => {
-    console.log(err)
+    res.sendStatus(400)
   })
 })
+
+app.post('/user/auth', async function (req, res) {
+  console.log(typeof (req.query.name))
+  console.log(typeof (req.query.password))
+
+  const collection = app.locals.collection
+  await collection.findOne({ "username": req.query.name, "password": req.query.password })
+    .then((document) => {
+      res.send(document)
+    })
+    .catch((err) => {
+      res.sendStatus(400)
+      console.log(err)
+    })
+})
+
 let io = require("socket.io")(http, {
   cors: {
     origin: "http://localhost:8080",
